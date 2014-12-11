874d2a7a53bcd41e69bca421a5dd6ea3722bf821
ChuongQuoc <chuonglaquoc@gmail.com>
2014-12-11 16:17:10 +0700

    mapkit demo
    demo version

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapkitDemo
+//
+//  Created by Admin on 12/10/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
